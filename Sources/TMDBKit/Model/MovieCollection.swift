a3bced3c6c20873002d5f59c3c7175be7a594c55
Kirill Pahnev <kirill.pahnev@gmail.com>
2021-06-01 16:57:13 +0300

    Fix parsing collection items with missing image paths

--- Sources/TMDBKit/Model/MovieCollection.swift
+++ Sources/TMDBKit/Model/MovieCollection.swift
@@ -10,6 +10,6 @@ import Foundation
 public struct MovieCollection: CodableEquatable {
     public let id: Int
     public let name: String
-    public let posterPath: String
-    public let backdropPath: String
+    public let posterPath: String?
+    public let backdropPath: String?
 }
