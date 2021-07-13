c584b6a0925a02b6c273ab372d5b868df073c43c
Kirill Pahnev <kirill.pahnev@gmail.com>
2021-07-13 11:15:55 +0300

    Parse new fields in reviews

--- Sources/TMDBKit/Model/Review.swift
+++ Sources/TMDBKit/Model/Review.swift
@@ -8,8 +8,17 @@
 import Foundation
 
 public struct Review: CodableEquatable {
+    public struct AuthorDetails: CodableEquatable {
+        public let avatarPath: String?
+        public let name: String
+        public let rating: Int?
+    }
+
     public let id: String
     public let author: String
+    public let authorDetails: AuthorDetails
     public let content: String
+    public let createdAt: String
+    public let updatedAt: String
     public let url: String
 }
