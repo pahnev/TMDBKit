d03ae0e97339ec96d64d18290860c8a6e384a6f5
Kirill Pahnev <kirill.pahnev@gmail.com>
2022-02-24 10:33:31 +0200

    Update credits fixtures and models

--- Sources/TMDBKit/Model/Cast.swift
+++ Sources/TMDBKit/Model/Cast.swift
@@ -8,21 +8,29 @@
 import Foundation
 
 public struct Cast: CodableEquatable {
-    public let castId: Int
+    public let adult: Bool
     public let character: String
-    public let gender: Int?
+    public let creditId: String
+    public let gender: Int
     public let id: Int
+    public let knownForDepartment: String
     public let name: String
+    public let originalName: String
     public let order: Int
+    public let popularity: Double
     public let profilePath: String?
 }
 
 public struct TVCast: CodableEquatable {
+    public let adult: Bool
     public let character: String
     public let creditId: String
-    public let gender: Int?
+    public let gender: Int
     public let id: Int
+    public let knownForDepartment: String
     public let name: String
+    public let originalName: String
     public let order: Int
+    public let popularity: Double
     public let profilePath: String?
 }
